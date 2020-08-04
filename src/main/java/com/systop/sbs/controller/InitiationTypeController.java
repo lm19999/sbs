847c085a9cf158a89cc6031ae95ccd22983712dd
lm19999 <2623064292@qq.com>
2020-08-04 11:39:29 +0800

    添加跨域注解

--- src/main/java/com/systop/sbs/controller/InitiationTypeController.java
+++ src/main/java/com/systop/sbs/controller/InitiationTypeController.java
@@ -4,6 +4,7 @@ import com.systop.sbs.common.pojo.InitiationType;
 import com.systop.sbs.common.util.SbsResult;
 import com.systop.sbs.service.InitiationTypeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @Date: 2020/7/29 20:57
  **/
 @RestController
+@CrossOrigin
 @RequestMapping("/initiationType")
 public class InitiationTypeController {
 
