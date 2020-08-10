e59c066ec26f8613d5a66d8be33e2ccaca3eea54
heart527 <1572910052@qq.com>
2020-08-10 15:53:13 +0800

    项目结构修改

--- src/main/java/com/systop/sbs/common/util/redis/RedisUtils.java
+++ src/main/java/com/systop/sbs/common/util/redis/RedisUtils.java
@@ -1,4 +1,4 @@
-package com.systop.sbs.common.util;
+package com.systop.sbs.common.util.redis;
 
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
